cc4e7485751e27646f1582ee7cb1d51fe5150145
Ronald Santos <ronaldcontact2019@gmail.com>
2023-06-30 00:20:38 -0300

    #001 CRUD - Create

---
+++ envia.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recebe Dados</title>
+</head>
+<body>
+    <?php
+        $conexao = mysqli_connect("localhost","root","","teste");
+        
+        // Checar conexão
+        if(!$conexao){
+            echo"Não conectado";
+        }
+        echo"Conectado ao banco>>>>>>>>>";
+
+        // Recuperar e validar CPF
+        $cpf = $_POST['cpf'];
+        $cpf = mysqli_real_escape_string($conexao, $cpf);
+        
+        $sql = "SELECT cpf  FROM teste.dados WHERE cpf='$cpf'";
+        $retorno = mysqli_query($conexao,$sql);
+        
+        if(mysqli_num_rows($retorno)>0) {
+            echo"CPF JÁ CADASTRADO!<br>";
+            echo"<a href='cadastro_usuario.html'>VOLTAR</a>";
+        }else{
+            $cpf = $_POST['cpf'];
+            $idade = $_POST['idade'];
+            $nome = $_POST['nome'];
+
+            $sql = "INSERT INTO teste.dados(cpf, idade, nome) values('$cpf','$idade','$nome')";
+            $retorno = mysqli_query($conexao,$sql);
+            echo">>>USUÁRIO CADASTRADO COM SUCESSO!<br>";
+            echo"<a href='cadastro_usuario.html'>VOLTAR</a>";
+        }
+    ?>
+</body>
+</html>
