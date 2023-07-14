1e397902b91864527b298e53691905833435dad5
Ronald Santos <ronaldcontact2019@gmail.com>
2023-07-13 23:57:19 -0300

    ex_variavel_constante

---
+++ cursophp/ex_variavel_000.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo de variável e constante</title>
+</head>
+<body>
+    <h1>Exemplo de variável e constante</h1>
+    <?php
+        $nome = "Ronald";
+        $sobrenome = "Santos";
+        const PAIS = "Brasil";
+
+        echo "Seja bem vindo $nome $sobrenome! Você mora no " . PAIS .".";
+    ?>
+</body>
+</html>
